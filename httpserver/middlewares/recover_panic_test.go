a176ce0c8a03b84ed545081b0d698667c78b7d1c
Dmitry Yu Okunev <xaionaro@dx.center>
2019-09-05 14:21:51 +0300

    Removed an extra output while "go test ./..."

--- httpserver/middlewares/recover_panic_test.go
+++ httpserver/middlewares/recover_panic_test.go
@@ -1,14 +1,21 @@
 package middlewares
 
 import (
+	"io/ioutil"
+	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/valyala/fasthttp"
+
+	"github.com/xaionaro-go/unsafetools"
 )
 
 func TestRecoverPanic(t *testing.T) {
 	ctx := &fasthttp.RequestCtx{}
+	*unsafetools.FieldByName(ctx, `s`).(**fasthttp.Server) = &fasthttp.Server{
+		Logger: log.New(ioutil.Discard, ``, 0),
+	}
 
 	RecoverPanic(func(ctx *fasthttp.RequestCtx) {
 		panic(`test`)
