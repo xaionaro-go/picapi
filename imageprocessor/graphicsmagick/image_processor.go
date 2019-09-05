ef2db9226b0a1807e333c617c1ba6597b29c007f
Dmitry Yu Okunev <xaionaro@dx.center>
2019-09-05 17:52:54 +0300

    Don't panic on signal.Interrupt

--- imageprocessor/graphicsmagick/image_processor.go
+++ imageprocessor/graphicsmagick/image_processor.go
@@ -1,11 +1,24 @@
 package imageprocessorgraphicsmagick
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/gographics/gmagick"
 )
 
 func init() {
 	gmagick.Initialize()
+
+	signal.Ignore(os.Interrupt)
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	go func() {
+		<-ch
+		os.Exit(int(syscall.EINTR))
+	}()
 }
 
 // ImageProcessor is a tool to manipulate images
